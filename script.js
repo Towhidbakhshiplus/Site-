0ae2f5fcd76bfdc838b4f86e27c124e2be1aa824
Towhidbakhshiplus <Towhidbakhshi@gmail.com>
2025-02-14 20:33:00 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+let currentIndex = 0;
+const images = document.querySelectorAll('.gallery img');
+
+function showNextImage() {
+    images[currentIndex].style.display = 'none';
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].style.display = 'block';
+}
+
+setInterval(showNextImage, 3000); // هر ۳ ث
